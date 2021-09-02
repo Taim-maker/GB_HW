dd8dd10ddf0761548e747d65450dd04d93a22128
Taim-maker <btu24@icloud.com>
2021-09-02 19:39:48 +0300

    212

---
+++ GB_HW/13.py
@@ -0,0 +1,57 @@
+# number = int(input('Введите целое число от 0 до 9: '))
+# while number < 10:
+#     print(number)
+#     number = number + 1
+# print('программа завершена успешно')
+# a = 5
+# while a > 0:
+#     print("!")
+#     a = a + 1
+# i = 0
+# while True:
+#     i += 1
+#     if i >= 10:
+#         # инструкция break при выполнении немедленно заканчивает выполнение цикла
+#         break
+#     if i % 2 == 0:
+#         # переходим к проверке условия цикла,
+#         # пропуская все операторы за инструкцией
+#         continue
+#     print(i)
+#     #i += 1
+# name = input("Enter your name: ")
+# print("Hello, %s!" % name)
+# print("Hello," , name)
+my_str = "text"
+mu_numb = 540
+# print('%s %d' % (my_str, mu_numb))
+# print('{}'.format(['el_1', 'el_2', 'el_3', 'el_4']))
+# print("{} {} {:>20}".format('my_param_1', 'my_param_2', 'my_param_3'))
+# print("{} {:<20} {:>20}".format('my_param_1', 'my_param_2', 'my_param_3'))
+# ip = '192.168.1.4'
+# mask = 10
+#
+# print(f"ip-params: {ip}, mask: {mask}")
+#
+# octets = ['10', '1', '1', '1']
+# mask = 10
+#
+# print(f"ip-params: {'.'.join(octets)}, mask: {mask}")
+#
+# oct1, oct2, oct3, oct4 = [10, 1, 1, 1]
+#
+# print(f'''IP address: {oct1:>8} {oct2:>8} {oct3:>8} {oct4:>8}''')
+def my_func(my_var):
+    my_var += 1
+    print(my_var)
+
+my_var = 10
+my_func(my_var)
+
+
+
+
+
+
+
+
